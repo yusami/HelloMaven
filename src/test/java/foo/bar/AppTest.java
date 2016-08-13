37c7af95d03870fab42f8ca02a0b3c64f5314e4f
Yasushi Usami <usami2go@yahoo.co.jp>
2016-08-13 20:49:20 +0900

    Added the test by DataPoint in JUnit.

--- src/test/java/foo/bar/AppTest.java
+++ src/test/java/foo/bar/AppTest.java
@@ -3,16 +3,22 @@ package foo.bar;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(Theories.class)
 public class AppTest
 {
-  /**
-   * Create the test target.
-   */
-	private App app = new App();
+    /**
+    * Create the test target.
+    */
+	  private App app = new App();
 
     /**
      * Simple Test.
@@ -40,5 +46,18 @@ public class AppTest
         App.main(args);
         // verify
         assertTrue( true );
+    }
+
+    @Theory
+    public void helloTheory() {
+        System.out.println("Hello, Theory!");
+    }
+
+    @DataPoints
+    public static String[] NAMES = { "Red", "Green", "Blue" };
+
+    @Theory
+    public void helloDataPoint(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
